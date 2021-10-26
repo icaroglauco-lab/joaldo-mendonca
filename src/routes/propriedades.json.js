5bc83e8fabe2219269454127e351bfbe4f4f0078
icaroglauco-lab <icaroglauco@gmail.com>
2021-10-26 15:50:45 -0300

    data endpoint

--- src/routes/propriedades.json.js
+++ src/routes/propriedades.json.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 export function get() {
-    let data = fs.readdirSync(`src/propriedades`)
+    let data = fs.readdirSync(`src/propriedades/`)
         .map(fileName => {
             const f = JSON.parse(fs.readFileSync(`src/propriedades/${fileName}`,
             {encoding:'utf8', flag:'r'}));
