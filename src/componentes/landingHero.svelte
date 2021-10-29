e43c74a25e9e25e4be3e651fb88597806a830969
icaroglauco-lab <icaroglauco@gmail.com>
2021-10-29 12:06:54 -0300

    dev

---
+++ src/componentes/landingHero.svelte
@@ -0,0 +1,14 @@
+<div class="hero min-h-screen" style="background-image: url(&quot;https://images.unsplash.com/photo-1494526585095-c41746248156?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80&quot;);">
+    <div class="hero-overlay bg-opacity-60"></div> 
+    <div class="text-center hero-content text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">
+              Hello there
+            </h1> 
+        <p class="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p> 
+        <button class="btn btn-primaria rounded-none">Vá rapidamente</button>
+      </div>
+    </div>
+  </div>
